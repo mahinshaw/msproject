ab54fe213d19d0db9d26d4917fc15aa489e208dc
tshetob <tsh.tobey@gmail.com>
2013-07-11 15:48:14 -0400

    XML reader

---
+++ KB/src/XMLinterface/Driver.java
@@ -0,0 +1,12 @@
+package XMLinterface;
+
+/**
+ * User: Tshering Tobgay
+ * Date: 7/11/13
+ */
+public class Driver {
+    public static void main(String arg[]){
+         Interface dri = new Interface();
+         dri.xmlInterface();
+    }
+}
