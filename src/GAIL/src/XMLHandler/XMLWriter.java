a90836048d22fb8fb818055e5de3e99d88e4cf15
mahinshaw <mahinshaw@gmail.com>
2013-07-22 19:35:30 -0400

    updates

--- src/GAIL/src/XMLHandler/XMLWriter.java
+++ src/GAIL/src/XMLHandler/XMLWriter.java
@@ -18,18 +18,32 @@ public class XMLWriter {
 
     public void writeXML(ArrayList<ArgStructure> argArray){
         int argCount = argArray.size();
+        int index = 0;
 
         try{
             DocumentBuilderFactory docFac = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = docFac.newDocumentBuilder();
 
             Document document = builder.newDocument();
+            Element session = document.createElement("Session");
+            //set root element
+            document.appendChild(session);
+
+            // append the question to the session
             Element question = document.createElement("Question");
+            session.appendChild(question);
+            question.appendChild(document.createTextNode(argArray.get(index).getQuestions().get(index)));
+
             Element argument = document.createElement("Argument");
-            Attr argIndex = document.createAttribute("arg");
             Element hypothesis = document.createElement("Hypothesis");
             Element data = document.createElement("Data");
             Element generalization = document.createElement("Generalization");
+
+            for (ArgStructure arg : argArray){
+                session.appendChild(argument);
+                argument.setAttribute("arg", "index");
+
+            }
         }
         catch (Exception e){
             e.printStackTrace();
