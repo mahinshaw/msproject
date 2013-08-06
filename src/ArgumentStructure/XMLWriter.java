b75aaab4a2071ea6e9744af95c3731aed12ab00b
mahinshaw <mahinshaw@gmail.com>
2013-08-06 16:25:50 -0400

    writeXML dot java

---
+++ src/ArgumentStructure/XMLWriter.java
@@ -0,0 +1,18 @@
+package ArgumentStructure;
+
+/**
+ * User: Mark Hinshaw
+ * Email: mahinshaw@gmail.com
+ * Date: 8/6/13
+ * github: https://github.com/mahinshaw/msproject
+ *
+ * This class handles writitng xml documents based upon the Argument Tree.
+ *
+ */
+public class XMLWriter {
+
+
+    public void writeXML(ArgumentTree tree){
+
+    }
+}
