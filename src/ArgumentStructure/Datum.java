e35de26886acb6544ec78383a8b094e26b626ca1
mahinshaw <mahinshaw@gmail.com>
2014-01-14 15:10:34 -0500

    update Datum to act as conjunction/empty when desirable

--- src/ArgumentStructure/Datum.java
+++ src/ArgumentStructure/Datum.java
@@ -7,14 +7,28 @@ package ArgumentStructure;
  * github: https://github.com/mahinshaw/msproject
  */
 public class Datum {
+    private final boolean isConjunction;
     private final String KBNODEID;
     private final String TEXT;
     private final char TYPE = 'D';
 
+    /**
+     * This constructor is used to create a conjunction datum.
+     * This type of datum will be used to denote the availability of multiple children.
+     */
+    public Datum(boolean isConjunction){
+        this.isConjunction = isConjunction;
+        this.KBNODEID = null;
+        this.TEXT = null;
+    }
+
     public Datum(String kbNodeID, String text){
+        this.isConjunction = false;
         this.KBNODEID = kbNodeID;
         this.TEXT = text;
     }
+
+    public boolean isConjunction() { return isConjunction; }
 
     public String getKBNODEID() {
         return KBNODEID;
