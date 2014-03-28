427841766c769640ed4a1be05db5309984e322b7
tshetob <tsh.tobey@gmail.com>
2014-03-28 09:59:48 -0400

    Minute change to ArgGen Driver

--- src/ArgumentGenerator/ArgGenDriver.java
+++ src/ArgumentGenerator/ArgGenDriver.java
@@ -5,8 +5,8 @@ import ArgumentGenerator.ArgGenerator.ArgGen;
 /**
  * User: Tshering Tobgay
  * Date: 3/27/14
- * This driver can test ArgGen capabilities without going through GAIL.
- * A much faster approach to debugging and soultion check.
+ * This driver can test ArgGen capabilities without going through comparator.
+ * A much faster approach to debugging and solution check.
  */
 public class ArgGenDriver {
     public static void main(String[] args){
